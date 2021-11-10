d3ceb0c2b1a4ef4157d8f0099057f97f85f5222d
Rahmathidayat4299 <87407102+Rahmathidayat4299@users.noreply.github.com>
2021-11-10 14:42:30 +0700

    Update and rename controller.kt to Controller.kt

---
+++ src/com/gamePertamaSaya/Controller.kt
@@ -0,0 +1,24 @@
+package com.gamePertamaSaya
+
+class Controller(private val callback: Callback) : interfacecontroller{
+
+
+
+    override fun check(playerOne: String, playerTwo: String){
+
+
+        if (playerOne == playerTwo) {
+            callback.pemenang("hasil imbang antara dua pemain")
+        } else if ((playerOne == "batu") && (playerTwo == "Gunting") || (playerOne == "Gunting") && (playerTwo == "kertas") || (playerOne == "kertas") && (playerTwo == "batu")) {
+
+            callback.pemenang("Selamat playerOne memang dan playerTwo kalah")
+
+        } else if ((playerOne == "batu") && (playerTwo == "kertas") || (playerOne == "Gunting") && (playerTwo == "batu") || (playerOne == "kertas") && (playerTwo == "Gunting")) {
+
+            callback.pemenang("Selamat playerTwo memang dan playerOne kalah")
+        } else {
+            callback.pemenang("maaf pilihan anda tidak tersedia")
+        }
+
+    }
+}
